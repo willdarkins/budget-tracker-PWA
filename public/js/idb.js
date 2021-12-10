638acc5fe958e939ad0f0ffbfb59d7ceebe1f5c8
Will Darkins <willdarkins@gmail.com>
2021-12-10 14:33:13 -0800

    added more functionality to idb.js file to store data offline

--- public/js/idb.js
+++ public/js/idb.js
@@ -10,19 +10,74 @@ request.onupgradeneeded = function (event) {
     db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 // upon a successful 
-request.onsuccess = function(event) {
+request.onsuccess = function (event) {
     /* when db is successfully created with its object store
     (from onupgradedneeded event above) or simply established a connection,
     save reference to db in global variable*/
     db = event.target.result;
-  
-    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+
+    // check if app is online
     if (navigator.onLine) {
-      // uploadPizza();
+        // uploadPizza();
     }
-  };
-  
-  request.onerror = function(event) {
+};
+
+request.onerror = function (event) {
     // log error here
     console.log(event.target.errorCode);
+};
+
+// This function will be executed if we attempt to submit a new transaction and there's no internet connection
+function saveRecord(record) {
+    // open a new transaction with the database with read and write permissions 
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+    // access the object store for `new_pizza`
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+
+    // add record to your store with add method
+    budgetObjectStore.add(record);
+}
+
+function uploadTransaction() {
+    // open a transaction on your db
+    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('new_transaction');
+  
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+  
+    // upon a successful .getAll() execution, run this function
+getAll.onsuccess = function() {
+    // if there was data in indexedDb's store, let's send it to the api server
+    if (getAll.result.length > 0) {
+      fetch('/api/pizzas', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          // open one more transaction
+          const transaction = db.transaction(['new_pizza'], 'readwrite');
+          // access the new_pizza object store
+          const pizzaObjectStore = transaction.objectStore('new_pizza');
+          // clear all items in your store
+          pizzaObjectStore.clear();
+
+          alert('All saved pizza has been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
   };
+  }
