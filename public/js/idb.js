e9a3d263bf3c6e57eb29c37fb629a1ea0feff749
Will Darkins <willdarkins@gmail.com>
2021-12-10 15:48:53 -0800

    data being stored in indexeddb but does not actually show up on the chrome page

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,56 +1,56 @@
 // create variable to hold db connection
 let db;
-// establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
+// establish a connection to IndexedDB database called 'budget' and set it to version 1
 const request = indexedDB.open('budget', 1);
 // this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
-    // save a reference to the database 
-    const db = event.target.result;
-    // table called `new_transaction`, set it to have an auto incrementing primary key for each new set of data inserted
-    db.createObjectStore('new_transaction', { autoIncrement: true });
+  // save a reference to the database 
+  const db = event.target.result;
+  // table called `new_transaction`, set it to have an auto incrementing primary key for each new set of data inserted
+  db.createObjectStore('new_transaction', { autoIncrement: true });
 };
 // upon a successful 
 request.onsuccess = function (event) {
-    /* when db is successfully created with its object store
-    (from onupgradedneeded event above) or simply established a connection,
-    save reference to db in global variable*/
-    db = event.target.result;
+  /* when db is successfully created with its object store
+  (from onupgradedneeded event above) or simply established a connection,
+  save reference to db in global variable*/
+  db = event.target.result;
 
-    // check if app is online
-    if (navigator.onLine) {
-         uploadTransaction()
-    }
+  // check if app is online
+  if (navigator.onLine) {
+    uploadTransaction()
+  }
 };
 
 request.onerror = function (event) {
-    // log error here
-    console.log(event.target.errorCode);
+  // log error here
+  console.log(event.target.errorCode);
 };
 
 // This function will be executed if we attempt to submit a new transaction and there's no internet connection
 function saveRecord(record) {
-    // open a new transaction with the database with read and write permissions 
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
+  // open a new transaction with the database with read and write permissions 
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
 
-    // access the object store for `new_pizza`
-    const budgetObjectStore = transaction.objectStore('new_transaction');
+  // access the object store for `new_transaction`
+  const budgetObjectStore = transaction.objectStore('new_transaction');
 
-    // add record to your store with add method
-    budgetObjectStore.add(record);
+  // add record to your store with add method
+  budgetObjectStore.add(record);
 }
 
 function uploadTransaction() {
-    // open a transaction on your db
-    const transaction = db.transaction(['new_transaction'], 'readwrite');
-  
-    // access your object store
-    const budgetObjectStore = transaction.objectStore('new_transaction');
-  
-    // get all records from store and set to a variable
-    const getAll = budgetObjectStore.getAll();
-  
-    // upon a successful .getAll() execution, run this function
-getAll.onsuccess = function() {
+  // open a transaction on your db
+  const transaction = db.transaction(['new_transaction'], 'readwrite');
+
+  // access your object store
+  const budgetObjectStore = transaction.objectStore('new_transaction');
+
+  // get all records from store and set to a variable
+  const getAll = budgetObjectStore.getAll();
+
+  // upon a successful .getAll() execution, run this function
+  getAll.onsuccess = function () {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
       fetch('/api/transaction', {
@@ -68,7 +68,7 @@ getAll.onsuccess = function() {
           }
           // open one more transaction
           const transaction = db.transaction(['new_transaction'], 'readwrite');
-          // access the new_pizza object store
+          // access the new_transaction object store
           const budgetObjectStore = transaction.objectStore('new_transaction');
           // clear all items in your store
           budgetObjectStore.clear();
@@ -80,5 +80,5 @@ getAll.onsuccess = function() {
         });
     }
   };
-  }
-  window.addEventListener('online', uploadTransaction);
+}
+window.addEventListener('online', uploadTransaction);
