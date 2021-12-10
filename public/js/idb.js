b3f636fe822f51c229e74c6a1c47582b5e2b821d
Will Darkins <willdarkins@gmail.com>
2021-12-10 11:39:10 -0800

    added basic object store code to ibd.js... still working to get everything connected

--- public/js/idb.js
+++ public/js/idb.js
@@ -2,3 +2,27 @@
 let db;
 // establish a connection to IndexedDB database called 'pizza_hunt' and set it to version 1
 const request = indexedDB.open('budget', 1);
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
+request.onupgradeneeded = function (event) {
+    // save a reference to the database 
+    const db = event.target.result;
+    // table called `new_transaction`, set it to have an auto incrementing primary key for each new set of data inserted
+    db.createObjectStore('new_transaction', { autoIncrement: true });
+};
+// upon a successful 
+request.onsuccess = function(event) {
+    /* when db is successfully created with its object store
+    (from onupgradedneeded event above) or simply established a connection,
+    save reference to db in global variable*/
+    db = event.target.result;
+  
+    // check if app is online, if yes run uploadPizza() function to send all local db data to api
+    if (navigator.onLine) {
+      // uploadPizza();
+    }
+  };
+  
+  request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+  };
