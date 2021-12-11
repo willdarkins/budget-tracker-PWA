42081bd5b1cd151c14ad00ed0709db94948f3ba5
Will Darkins <willdarkins@gmail.com>
2021-12-10 21:16:31 -0800

    code to intercept fetch requests

--- public/service-worker.js
+++ public/service-worker.js
@@ -45,3 +45,43 @@ self.addEventListener('activate', function(evt) {
     )
     self.clients.claim();
 })
+
+// Intercept fetch requests
+self.addEventListener('fetch', function(evt) {
+    if (evt.request.url.includes('/api/')) {
+        evt.respondWith(
+          caches
+            .open(DATA_CACHE_NAME)
+            .then(cache => {
+              return fetch(evt.request)
+                .then(response => {
+                  // If the response was good, clone it and store it in the cache.
+                  if (response.status === 200) {
+                    cache.put(evt.request.url, response.clone());
+                  }
+    
+                  return response;
+                })
+                .catch(err => {
+                  // Network request failed, try to get it from the cache.
+                  return cache.match(evt.request);
+                });
+            })
+            .catch(err => console.log(err))
+        );
+    
+        return;
+      }
+      evt.respondWith(
+        fetch(evt.request).catch(function() {
+          return caches.match(evt.request).then(function(response) {
+            if (response) {
+              return response;
+            } else if (evt.request.headers.get('accept').includes('text/html')) {
+              // return the cached home page for all requests for html pages
+              return caches.match('/');
+            }
+          });
+        })
+      );
+});
