9089f5678c4e789220d8515cc9082020d7b55c69
Will Darkins <willdarkins@gmail.com>
2021-12-10 21:12:52 -0800

    listed files to cache and created variables for cache name and data_cache_name

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'my-site-cache-v2';
+const DATA_CACHE_NAME = 'data-cache-v2';
+
+const FILES_TO_CACHE = [
+    '/',
+    '/css/styles.css',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+    '/icons/icon-128x128.png',
+    '/js/idb.js',
+    '/js/index.js',
+    '/index.html',
+    '/manifest.json',
+  ];
